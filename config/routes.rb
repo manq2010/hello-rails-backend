aebcb16804e1004ecdd5c9cfd443584ff16c751e
Mancoba <manq2010@gmail.com>
2023-03-28 23:11:17 +0200

    Add backend api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get 'messages/random', to: 'messages#random'
+    end
+  end
+  
 end
